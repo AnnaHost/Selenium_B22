baa0aa6815c8da44b6ec9cc267f980bdcd06409e
Anna Host <roromia@mail.ru>
2021-02-25 17:42:25 +0300

    add login test

---
+++ login.py
@@ -0,0 +1,21 @@
+import time
+
+# webdriver это и есть набор команд для управления браузером
+from selenium import webdriver
+
+# инициализируем драйвер браузера. После этой команды вы должны увидеть новое открытое окно браузера
+driver = webdriver.Chrome()
+
+# команда time.sleep устанавливает паузу в 5 секунд, чтобы мы успели увидеть, что происходит в браузере
+time.sleep(5)
+
+# Метод get сообщает браузеру, что нужно открыть сайт по указанной ссылке
+driver.get("http://localhost/litecart/public_html/admin/")
+driver.find_element_by_name("username").send_keys("admin")
+driver.find_element_by_name("password").send_keys("admin")
+driver.find_element_by_name("login").click()
+
+time.sleep(5)
+
+# После выполнения всех действий мы не должны забыть закрыть окно браузера
+driver.quit()
