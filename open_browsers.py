7c393dc5875036497d9392dfb8db284b6b2c36e4
Anna Host <roromia@mail.ru>
2021-02-27 13:56:50 +0300

    some changes

--- open_browsers.py
+++ open_browsers.py
@@ -7,7 +7,7 @@ def open_browser(driver):
     driver.find_element_by_name("username").send_keys("admin")
     driver.find_element_by_name("password").send_keys("admin")
     driver.find_element_by_name("login").click()
-    time.sleep(5)
+    time.sleep(3)
     driver.quit()
 
 
